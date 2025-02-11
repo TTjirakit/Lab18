efc6108b8d34f34a95d60d2d3f3e7bb963c7948f
TTjirakit <captain112006@gmail.com>
2025-02-11 11:47:11 +0700

    completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,34 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1, Rect R2){
+    double left, right;
+    if (R1.x > R2.x) left = R1.x;
+    else left = R2.x;
 
+    if (R1.x + R1.w < R2.x + R2.w) right = R1.x + R1.w;
+    else right = R2.x + R2.w;
 
+    double overlap_width = right - left;
 
+    double top, bottom;
+    if (R1.y < R2.y) top = R1.y;
+    else top = R2.y;
+
+    if (R1.y - R1.h > R2.y - R2.h) bottom = R1.y - R1.h;
+    else bottom = R2.y - R2.h;
+
+    double overlap_height = top - bottom;
+
+    if (overlap_width <= 0 || overlap_height <= 0) return 0;
+    
+    return overlap_width * overlap_height;
+}
+
+int main() {
+    Rect R1 = {1,1,5,5};
+    Rect R2 = {2,2,5,5};
+    cout << overlap(R1, R2);
+
+    return 0;
 }
